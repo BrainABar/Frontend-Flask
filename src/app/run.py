c1257bac4b67d5d5f75430b127e18d649ca0963e
Bryan Bar <bryanbarajas@tutamail.com>
2020-07-20 02:27:06 -0700

    UPDATED ignore/comment

--- src/app/run.py
+++ src/app/run.py
@@ -3,6 +3,7 @@ from app import create_app
     This will run the flask app using the internal server
     It is not recommended to use it during production but useful during
     development.
+    Errors might be given if setup.py is not used since src/ path is needed
     > export FLASK_APP=app.run.py
     > export FLASK_ENV=development
     > flask run
