e09f356e694cef87424d0596ae3035e925d8dbb6
Bryan Bar <bryanbarajas@tutamail.com>
2020-07-20 14:49:31 -0700

    fails to see how workflows work when a Github Action fails

--- src/tests/test_personal.py
+++ src/tests/test_personal.py
@@ -2,3 +2,9 @@ def test_home(test_client):
 
     response = test_client.get('/')
     assert response.status_code == 200
+
+
+def test_contact(test_client):
+
+    response = test_client.get('/contact')
+    assert response.status_code == 404
