57d28d8f5cd60eb13c91d661dd6e6a5d1cd2228d
Bryan Bar <31219516+BrainABar@users.noreply.github.com>
2020-07-21 15:19:10 -0700

    Removing of contact page and added to default layout (#3)
    * contact page and now served by layout template
    
    * removed contact page from tests

--- src/tests/test_personal.py
+++ src/tests/test_personal.py
@@ -4,12 +4,6 @@ def test_home(test_client):
     assert response.status_code == 200
 
 
-def test_contact(test_client):
-
-    response = test_client.get('/contact')
-    assert response.status_code == 200
-
-
 def test_projects(test_client):
 
     response = test_client.get('/projects')
